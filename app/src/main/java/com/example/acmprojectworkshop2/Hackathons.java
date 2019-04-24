8f7e1a63b42fe0ba30c763f69e5f999cf71d99dc
bprerak <bhaktaprerak@gmail.com>
2019-04-23 17:27:29 -0700

    first cooment

---
+++ app/src/main/java/com/example/acmprojectworkshop2/Hackathons.java
@@ -0,0 +1,19 @@
+package com.example.acmprojectworkshop2;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Hackathons {
+
+    @SerializedName("array")
+    List<Event> events;
+
+    //List of events. We name it "getEvents" because it's the method
+    //That will return the events from this class.
+    public List<Event> getEvents() {
+
+        return events;
+    }
+
+}
